6e62eb2a1eec12208bacb9501c3f4bd32302af69
Niccu Bagonoc <nbagonoc@Niccus-MacBook-Air.local>
2018-05-25 08:05:18 +0800

    security issue

--- config/db_secret_dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleClientID:
-    "361351937629-1pg9al9945phgquqau8cov4fvp98s1qp.apps.googleusercontent.com",
-  googleClientSecret: "nHyHIF43h47250Vv4-yUwaCE",
-  mongoURI:
-    "mongodb://nbagonoc:Th3r3nc3n3w@ds233970.mlab.com:33970/project-three",
-  secretOrKey: "thesecretkey",
-  cookieKey: "jesejfpisejfsegseiesfhesfoeshfoseifhesowqiweqiwhrwrj"
-};
