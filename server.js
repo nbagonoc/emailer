95c27bc2a7bfd9442006e50bc1ee0dc2a4188fb9
Niccu Bagonoc <nbagonoc@Niccus-MacBook-Air.local>
2018-05-26 20:41:39 +0800

    yawa na bug

--- server.js
+++ server.js
@@ -42,14 +42,16 @@ require("./routes/authRoutes")(app);
 // billing routes
 require("./routes/billingRoutes")(app);
 
-// serve production assets
-app.use(express.static("client/build"));
-
-// routing for production
 if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("client/build"));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
   const path = require("path");
-  app.get("/*", (req, res) => {
-    res.sendfile(path.resolve(__dirname, "../client", "build", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
